e3ace3d8128fa65013d27399fda97d0e92a32256
Bhanu Singh <59247235+biasedzeus@users.noreply.github.com>
2022-09-08 16:32:42 +0530

    protected routes & toaster added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
-import Register from './pages/Register'
+import Register from "./pages/Register";
+import { Toaster } from "react-hot-toast";
+import { Route, Routes } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
+import ProtectedRoute from "./utils/ProtectedRoute";
+
 function App() {
-  const [count, setCount] = useState(0);
+  const { currentUser } = useContext(AuthContext);
+  console.log("App", currentUser);
 
   return (
     <div>
-      {/* <Register/>
-      <Login/> */}
-      <HomePage/>
+      <Routes>
+        <Route path="/">
+          <Route
+            index
+            element={
+              <ProtectedRoute user={currentUser}>
+                <HomePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="*" element={<div>ERROR 404</div>} />
+        </Route>
+      </Routes>
+      <Toaster />
     </div>
   );
 }
