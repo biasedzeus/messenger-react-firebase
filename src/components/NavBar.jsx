693093ae56838569ddb0ebc4c82b0755abfbbdc8
Bhanu Singh <59247235+biasedzeus@users.noreply.github.com>
2022-09-08 12:13:49 +0530

    toast added for logOut

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
-import avatar from '../assets/man.png'
-import { signOut } from 'firebase/auth';
-import { auth } from '../firebase';
-
+import React from "react";
+import avatar from "../assets/man.png";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import toast from 'react-hot-toast'
 
 const NavBar = () => {
   return (
-    <nav className='navbar'>
-       <span className='logo'> NavBar</span>
-       <div className="user">
-        <img src={avatar}  alt="" />
+    <nav className="navbar">
+      <span className="logo"> NavBar</span>
+      <div className="user">
+        <img src={avatar} alt="" />
         <span>John</span>
-        <button onClick={() => signOut(auth)}>Log Out</button>
-       </div>
-        
-        </nav>
-  )
-}
+        <button
+          onClick={() => {
+            const logOutToast = toast.loading("Logging Out",{
+              duration:4000,
+              position: "bottom-center",
+            })
+            signOut(auth);
+            toast.success("Logged Out SuccessFully",{
+              id:logOutToast
+            })
+          }}
+        >
+          Log Out
+        </button>
+      </div>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
