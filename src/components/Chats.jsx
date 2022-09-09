f9b8ffef207a7c0ce782b1044ab6c269894406d6
Bhanu Singh <59247235+biasedzeus@users.noreply.github.com>
2022-09-09 13:55:20 +0530

    drawer close on handleSelect

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -5,7 +5,7 @@ import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
 import { db } from "../firebase";
 
-const Chats = () => {
+const Chats = ({closeDrawer}) => {
   const { currentUser } = useContext(AuthContext);
   const {dispatch } = useContext(ChatContext);
   const [chats, setChats] = useState([]);
@@ -26,6 +26,7 @@ const Chats = () => {
 
   const handleSelect = (user) =>{
     dispatch({type:'CHANGE_USER',payload: user})
+    closeDrawer &&  closeDrawer();
     console.log(user);
 }
 
@@ -33,7 +34,7 @@ const Chats = () => {
     <div className="chats">
       {chats && Object.entries(chats)?.sort((a,b) => b[1].date - a[1].date ).map((chat) => (
         <div className="userchat" key={chat[0]} onClick={() =>handleSelect(chat[1].userInfo)}>
-          <img src={chat[1].userInfo.photoURL} alt="" />
+          <img className="userchatImage" src={chat[1].userInfo.photoURL} alt="" />
           <div className="userChatInfo">
             <span>{chat[1].userInfo.displayName}</span>
             <p>{chat[1].lastMessage ? chat[1].lastMessage?.text : ""}</p>
